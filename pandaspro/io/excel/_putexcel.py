c4cb51ca0f3e7017c7083059d9f5218d4abd486d
solu <45439609+soluentre@users.noreply.github.com>
2024-03-03 18:54:32 -0500

    putexcel dev

--- pandaspro/io/excel/_putexcel.py
+++ pandaspro/io/excel/_putexcel.py
@@ -8,14 +8,14 @@ from pandaspro import FramePro, resize
 
 
 class FramexlWriter:
-    '''
-    key attributes are the xl cell ranges in String
-    '''
 
-    def __init__(self, frame: DataFrame | FramePro,
-                 start_cell: str,
-                 index: bool = False,
-                 header: bool = True):
+    def __init__(
+        self, frame: DataFrame | FramePro,
+        start_cell: str,
+        index: bool = False,
+        header: bool = True,
+    ) -> None:
+
         header_row_count = len(frame.columns.levels) if isinstance(frame.columns, pd.MultiIndex) else 1
         index_column_count = len(frame.index.levels) if isinstance(frame.index, pd.MultiIndex) else 1
 
@@ -31,28 +31,35 @@ class FramexlWriter:
         elif header == False and index == False:
             tr, tc = frame.shape[0], frame.shape[1]
             export_data = frame.to_numpy().tolist()
+        else:
+            export_data = frame
 
         self.frame = export_data
         self.start_cell = start_cell
 
 
 class PutxlSet:
-    def __init__(self,
-                 workbook: str,
-                 sheet_name: str,
-                 noisily: bool = False):
+
+    def __init__(
+        self,
+        workbook: str,
+        sheet_name: str,
+        alwaysreplace: str = None,
+        noisily: bool = False
+    ) -> None:
 
         def _extract_filename_from_path(path):
             return Path(path).name
 
         def _get_open_workbook_by_name(name):
-            """Return the open workbook by its name if exists, otherwise return None."""
+            # Return the open workbook by its name if exists, otherwise return None
             for app in xw.apps:
                 for wb in app.books:
                     if wb.name == name:
                         return wb, app
             return None, None
 
+        # App and Workbook declaration
         wb, app = _get_open_workbook_by_name(_extract_filename_from_path(workbook))  # Check if the file is already open
         if wb:
             if noisily:
@@ -70,26 +77,66 @@ class PutxlSet:
         else:
             wb = xw.Book(workbook)
 
+        # Worksheet declaration
         current_sheets = [sheet.name for sheet in wb.sheets]
         if sheet_name in current_sheets:
-            if sheetreplace:
-                # Delete the existing sheet and create a new one
-                new_sheet = wb.sheets.add(after=wb.sheets.count)
-                wb.sheets[sheet_name].delete()
-                new_sheet.range(start_cell).value = export_data
-                new_sheet.name = sheet_name
-            else:
-                wb.sheets[sheet_name].range(start_cell).value = export_data
-    def putxl(self, start_cell: str,
-              frame,
-              index: bool = False,
-              header: bool = True):
-        io = FramexlWriter(frame=frame,
-                           start_cell=start_cell,
-                           index=index,
-                           header=header)
-        self.wb.range(io.start_cell).value = io.frame
+            sheet = wb.sheets[sheet_name]
+        else:
+            sheet = wb.sheets.add(after=wb.sheets.count)
+            sheet.name = sheet_name
 
+        self.workbook = workbook
+        self.worksheet = sheet_name
+        self.wb = wb
+        self.ws = sheet
+        self.globalreplace = alwaysreplace
+
+    def putxl(
+        self,
+        start_cell: str,
+        frame,
+        index: bool = False,
+        header: bool = True,
+        replace: str = None,
+        sheetreplace: bool = False,
+        sheet_name: str = None,
+        debug: bool = False
+    ) -> None:
+        io = FramexlWriter(frame = frame, start_cell = start_cell, index=index, header=header)
+        replace_type = self.globalreplace if self.globalreplace else replace
+
+        # If a sheet_name is specified at the very end, then override the current sheet
+        if sheet_name and sheet_name != self.worksheet:
+            if sheet_name in [sheet.name for sheet in self.wb.sheets]:
+                ws = self.wb.sheets[sheet_name]
+            else:
+                ws = self.wb.sheets.add(after=self.wb.sheets.count)
+                ws.sheet.name = sheet_name
+        else:
+            ws = self.ws
+
+        # If sheetreplace or replace is specified, then delete the old sheet and create a new one
+        if sheetreplace or replace_type == 'sheet':
+            _sheetmap = {sheet.index: sheet.name for sheet in self.wb.sheets}
+            original_index = ws.index
+            original_name = ws.name
+            total_count = self.wb.sheets.count
+            if debug:
+                print(f"Row 121: original index is {original_index} and ")
+            ws.delete()
+            if original_index == total_count:
+                new_sheet = self.wb.sheets.add(after=self.wb.sheets[_sheetmap[original_index - 1]])
+                if debug:
+                    print(f"Row 128: added after the sheet !'{_sheetmap[original_index - 1]}'")
+            else:
+                new_sheet = self.wb.sheets.add(before=self.wb.sheets[_sheetmap[original_index + 1]])
+                if debug:
+                    print(f"Row 132: added before the sheet !'{_sheetmap[original_index + 1]}'")
+            new_sheet.name = original_name
+            ws = new_sheet
+            self.ws = ws
+
+        ws.range(io.start_cell).value = io.frame
 
 if __name__ == '__main__':
     import numpy as np
@@ -111,8 +158,10 @@ if __name__ == '__main__':
         'GDP per Capita (USD)': gdp_per_capita.round(2)
     })
 
-    ps = PutxlSet('test.xlsx', 'Sheet1', noisily=True)
-    ps.putxl('A1', df)
+    ps = PutxlSet('test.xlsx', 'Sheet3', noisily=True)
+    ps.putxl('A1', df, index=False, header=False, sheetreplace=True, debug=True)
+
+
 '''
 putxl('A1', ...)
 putxlsheet('A1', ..., sheet='Sheet2')
