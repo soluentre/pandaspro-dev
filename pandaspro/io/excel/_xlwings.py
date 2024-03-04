fbdcd9e97e9fc46945e6bab5139ff6da37cede22
solu <45439609+soluentre@users.noreply.github.com>
2024-03-04 18:06:08 -0500

    putexcel dev

--- pandaspro/io/excel/_xlwings.py
+++ pandaspro/io/excel/_xlwings.py
@@ -1,8 +1,9 @@
 import xlwings as xw
-from win32com.client import constants as c
+import re
+
 
 class RangeOperator:
-    _alignment_map = alignment_options = alignment_options = {
+    _alignment_map = {
         'hcenter': ['h', xw.constants.HAlign.xlHAlignCenter],
         'center_across_selection': ['h', xw.constants.HAlign.xlHAlignCenterAcrossSelection],
         'hdistributed': ['h', xw.constants.HAlign.xlHAlignDistributed],
@@ -23,28 +24,56 @@ class RangeOperator:
 
     def format(
             self,
+            font: str | tuple = None,
+            font_name: str = None,
+            font_size: str = None,
+            font_color: str | tuple = None,
+            italic: bool = False,
+            bold: bool = False,
+            underline: bool = False,
+            strikeout: bool = False,
             align: str | list = None,
     ) -> None:
+        if font:
+            if isinstance(font, tuple):
+                self.range.font.color = font
+            elif isinstance(font, str):
+                for item in font.split():
+                    if re.match(item, '#\d{6}'):
+
+        # 'color' attribute can follow formats: (1) string of hex starts with '#'; (2) tuple of RGB
+        self.range.font.name = font_name
+        self.range.font.size = font_size
+        self.range.font.color = font_color
+        self.range.font.italic = italic
+        self.range.font.bold = bold
+        self.range.api.Font.Underline = underline
+        self.range.api.Font.Strikethrough = strikeout
+
         if align:
             def _alignfunc(alignkey):
                 if align in ['center', 'justify', 'distributed']:
-                    self.range.api.VerticalAlignment = self._alignment_map['v'+alignkey][1]
-                    self.range.api.HorizontalAlignment = self._alignment_map['h'+alignkey][1]
+                    self.range.api.VerticalAlignment = self._alignment_map['v' + alignkey][1]
+                    self.range.api.HorizontalAlignment = self._alignment_map['h' + alignkey][1]
                 elif self._alignment_map[alignkey][0] == 'v':
                     self.range.api.VerticalAlignment = self._alignment_map[alignkey][1]
                 elif self._alignment_map[alignkey][0] == 'h':
                     self.range.api.HorizontalAlignment = self._alignment_map[alignkey][1]
                 elif align not in self._alignment_map.keys():
                     raise ValueError(f'Alignment {alignkey} is not supported')
+                return
             if isinstance(align, str):
-                _alignfunc(align)
+                for item in align.split():
+                    _alignfunc(item)
             elif isinstance(align, list):
                 for item in align:
                     _alignfunc(item)
 
+        return
 
     def clear(self):
         self.range.clear()
+
 
 if __name__ == '__main__':
     wb = xw.Book('test.xlsx')
@@ -55,5 +84,5 @@ if __name__ == '__main__':
 
     # Step 3: Create an object of the RangeOperator class with the specified range
     a = RangeOperator(my_range)
-    b = a.format(align='distributed')
+    a.format(font_color='#FFFF00')
     print(a.range)
