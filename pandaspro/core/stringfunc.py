7f855a0eb0c2b842fd1ea2fa5e756108affcea99
solu <45439609+soluentre@users.noreply.github.com>
2024-03-15 12:48:11 -0400

    putexcel dev

--- pandaspro/core/stringfunc.py
+++ pandaspro/core/stringfunc.py
@@ -1,14 +1,15 @@
 import re
 
+
 def wildcardread(stringlist, varkey):
-    '''
+    """
     This is the wildcard reader function which can parse containing-wildcard varnames into meaningful list of varnames
     For example: mak* can return the list of ["make1", "make2", "make3"] which can be further used to slice dataframes
 
     :param stringlist: a list of vars with wildcards
     :param varkey: a variable key with wildcard in it to match one or more variables
     :return:
-    '''
+    """
     if '-' in varkey:
         crange = re.split(r'\s*-\s*', varkey)
         element1 = crange[0]
@@ -28,8 +29,8 @@ def wildcardread(stringlist, varkey):
         return matching_strings
 
 
-def str2list(inputstring: str) -> str:
-    '''
+def str2list(inputstring: str) -> list[str] | list[str | Any]:
+    """
     This function is used to turn a string of vars to a list object
     Python can not automatically parse list of vars as written in a string separated by space, like "make price mpg rep78" as comparing to Stata
     And this function will serve as the parser to separate the string with spaces into var/var with wildcard sections
@@ -38,7 +39,7 @@ def str2list(inputstring: str) -> str:
     Note: you can use three types of wildcard: * ? -, as supported with the wildcardread function
 
     :return: a list of varnames
-    '''
+    """
     pattern = r'\w+\s*-\s*\w+'
     match = re.findall(pattern, inputstring)
     if not match:
@@ -52,21 +53,27 @@ def str2list(inputstring: str) -> str:
     return newlist
 
 
-def parsewild(checklist: list, promptstring: str):
-    '''
+def parsewild(checklist: list, promptstring: str, dictmap: dict):
+    """
     This function will return the searched varnames from a python dataframe according to the prompt string
 
     :param checklist: list
     :param promptstring: for example: "name* title*"
+    :param dictmap: dictionary to convert abbr names
+
     :return: a list of available varnames
-    '''
+    """
     varlist = []
     result_list = []
     for varkey in str2list(promptstring):
-        varlist += wildcardread(checklist, varkey)
+        if varkey in dictmap.keys():
+            for term in dictmap[varkey]:
+                varlist += wildcardread(checklist, term)
+        else:
+            varlist += wildcardread(checklist, varkey)
     [result_list.append(x) for x in varlist if x not in result_list]
     return result_list
 
 
 def getvarlist(df, promptstring):
-    return parsewild(df.columns, promptstring)
+    return parsewild(df.columns, promptstring, 1)
