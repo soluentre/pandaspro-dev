094c764a0732a56531e2350167161b40e7be2662
Pear000 <94930479+Littlepear000@users.noreply.github.com>
2024-03-24 15:39:47 -0400

    cdformat

--- pandaspro/io/excel/_framewriter.py
+++ pandaspro/io/excel/_framewriter.py
@@ -3,7 +3,6 @@ from pandaspro.io.excel._utils import CellPro
 import pandas as pd
 
 
-
 class FramexlWriter:
 
     def __init__(
@@ -13,7 +12,7 @@ class FramexlWriter:
             index: bool = False,
             header: bool = True,
             column_list: list = None,
-            index_mask = None
+            index_mask=None
     ) -> None:
         if isinstance(content, str):
             self.content = content
@@ -34,30 +33,33 @@ class FramexlWriter:
 
             # Create a cells Map
             i = 0
-            for index, row in dfmap.iterrows():
+            for dfmap_index, row in dfmap.iterrows():
                 j = 0
                 for col in dfmap.columns:
-                    dfmap.loc[index, col] = dfmapstart.offset(i, j).cell
+                    dfmap.loc[dfmap_index, col] = dfmapstart.offset(i, j).cell
                     j += 1
                 i += 1
 
             self.formatrange = "Please provide the column_list/index_mask to select a sub-range"
-            self.valuerange
+
             if column_list:
                 if isinstance(column_list, str):
                     column_list = parsewild(column_list, dfmap.columns)
                 self.formatrange = dfmap[column_list]
             if index_mask:
                 self.formatrange = self.formatrange[index_mask]
+            print('runned to here')
 
             # Calculate the Ranges
             if header == True and index == True:
+                print('1')
                 tr, tc = content.shape[0] + header_row_count, content.shape[1] + index_column_count
                 export_data = content
                 range_index = cellobj.offset(header_row_count, 0).resize(tr - header_row_count, index_column_count)
                 range_indexnames = cellobj.resize(header_row_count, header_row_count)
                 range_header = cellobj.offset(0, index_column_count).resize(header_row_count, tc - index_column_count)
             elif header == False and index == True:
+                print('2')
                 tr, tc = content.shape[0], content.shape[1] + index_column_count
                 export_data = content.reset_index().to_numpy().tolist()
                 range_index = cellobj.resize(tr, index_column_count)
@@ -93,13 +95,16 @@ class FramexlWriter:
             self.range_index = range_index.cell if range_index != 'N/A' else 'N/A'
             self.range_header = range_header.cell if range_header != 'N/A' else 'N/A'
             self.range_indexnames = range_indexnames.cell if range_indexnames != 'N/A' else 'N/A'
-            self.range_top_checker = CellPro(self.cell).offset(-1, 0).resize(1, self.tc).cell if CellPro(self.cell).index_cell()[0] != 1 else None
+            self.range_top_checker = CellPro(self.cell).offset(-1, 0).resize(1, self.tc).cell if \
+            CellPro(self.cell).index_cell()[0] != 1 else None
             self.cellmap = dfmap
+
 
 if __name__ == '__main__':
     import pandas as pd
+
     countries = ["USA", "China", "Japan", "Germany", "India", "UK", "France", "Brazil", "Italy", "Canada"]
-    gdp = [11.43, 14.59, 12.45, 11.35, 9.05, 13.27, 9.31, 17.94, (1,2,3,4,5,6), 8.29]
+    gdp = [11.43, 14.59, 12.45, 11.35, 9.05, 13.27, 9.31, 17.94, (1, 2, 3, 4, 5, 6), 8.29]
     population = [1110.5, 745.2, 799.6, 1296.6, 108.7, 131.1, 38.1, 1167.3, 1091.6, 1219.3]
     df = pd.DataFrame({
         'Country': countries,
@@ -110,6 +115,7 @@ if __name__ == '__main__':
     # print(FramexlWriter(df, 'A1', index=True).range_index)
 
     import xlwings as xw
+
     ws = xw.Book('test.xlsx').sheets['FF']
     ws.range('G1').value = df
     a = FramexlWriter(df, 'G1', column_list='Country')
@@ -124,4 +130,3 @@ if __name__ == '__main__':
             }
         }
     }
-
