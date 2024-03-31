a67595db29a01bdb5caacbfe3ab55ba01f7e8c88
solu <45439609+soluentre@users.noreply.github.com>
2024-03-31 11:57:29 -0400

    update

--- pandaspro/sampledf/api.py
+++ pandaspro/sampledf/api.py
@@ -1,10 +1,12 @@
 from pandaspro.sampledf.sampledf import (
     sysuse_countries,
-    sysuse_auto
+    sysuse_auto,
+    wbuse_pivot
 )
 
 
 __all__ = [
     'sysuse_countries',
-    'sysuse_auto'
+    'sysuse_auto',
+    'wbuse_pivot'
 ]
