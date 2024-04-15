0ba0e3169adc102dea425bc9ed73308354e42bcf
solu <45439609+soluentre@users.noreply.github.com>
2024-04-15 09:50:37 -0400

    fix index_wiring

--- pandaspro/io/excel/_cdformat.py
+++ pandaspro/io/excel/_cdformat.py
@@ -6,18 +6,21 @@ mytools = toolObject()
 
 
 def df_with_index_for_mask(df):
-    rename_index = {item: f'__myindex_{str(i)}' for i, item in enumerate(df.index.names)}
-    rename_index_back = {f'__myindex_{str(i)}': item for i, item in enumerate(df.index.names)}
-    index_preparing = df.reset_index()
-    index_wiring = index_preparing.rename(columns=rename_index)
-    for column in df.index.names:
-        index_wiring[column] = index_preparing[column]
-    index_wiring = index_wiring.set_index(list(rename_index.values()))
-    index_wiring.index.names = [rename_index_back.get(name) for name in index_wiring.index.names]
-    reorder_columns = list(df.index.names) + list(df.columns)
-    index_wiring = index_wiring[reorder_columns]
+    if df.index.name is not None:
+        rename_index = {item: f'__myindex_{str(i)}' for i, item in enumerate(df.index.names)}
+        rename_index_back = {f'__myindex_{str(i)}': item for i, item in enumerate(df.index.names)}
+        index_preparing = df.reset_index()
+        index_wiring = index_preparing.rename(columns=rename_index)
+        for column in df.index.names:
+            index_wiring[column] = index_preparing[column]
+        index_wiring = index_wiring.set_index(list(rename_index.values()))
+        index_wiring.index.names = [rename_index_back.get(name) for name in index_wiring.index.names]
+        reorder_columns = list(df.index.names) + list(df.columns)
+        index_wiring = index_wiring[reorder_columns]
 
-    return index_wiring
+        return index_wiring
+    else:
+        return df
 
 
 class CdFormat:
