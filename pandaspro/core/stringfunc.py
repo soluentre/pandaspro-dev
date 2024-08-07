efefbfe5e370caa0622aac73dcbd1cd7700de698
Ry <rd1170@georgetown.edu>
2024-08-06 19:34:34 -0600

    revise str2list (header)

--- pandaspro/core/stringfunc.py
+++ pandaspro/core/stringfunc.py
@@ -29,31 +29,31 @@ def wildcardread(stringlist, varkey):
         matching_strings = [s for s in stringlist if regex.match(s)]
         return matching_strings
 
-
-def str2list(inputstring: str) -> Union[List[str], List[Union[str, Any]]]:
-    """
-    This function is used to turn a string of vars to a list object
-    Python can not automatically parse list of vars as written in a string separated by space, like "make price mpg rep78" as comparing to Stata
-    And this function will serve as the parser to separate the string with spaces into var/var with wildcard sections
-
-    :param inputstring: the key input a string with many varnames separated by X number of spaces
-    Note: you can use three types of wildcard: * ? -, as supported with the wildcardread function
-
-    :return: a list of varnames
-    """
-    pattern = r'\w+\s*--\s*\w+'
-    match = re.findall(pattern, inputstring)
-    if not match:
-        newlist = [s.strip() for s in inputstring.split(';')]
-    else:
-        for index, item in enumerate(match):
-            inputstring = inputstring.replace(item, '__' + str(index) + '__')
-        aloneitem = inputstring.split(';')
-        for index, item in enumerate(match):
-            newlist = [item if s.strip() == '__' + str(index) + '__' else s.strip() for s in aloneitem]
-
-    # noinspection PyUnboundLocalVariable
-    return newlist
+#
+# def str2list(inputstring: str) -> Union[List[str], List[Union[str, Any]]]:
+#     """
+#     This function is used to turn a string of vars to a list object
+#     Python can not automatically parse list of vars as written in a string separated by space, like "make price mpg rep78" as comparing to Stata
+#     And this function will serve as the parser to separate the string with spaces into var/var with wildcard sections
+#
+#     :param inputstring: the key input a string with many varnames separated by X number of spaces
+#     Note: you can use three types of wildcard: * ? -, as supported with the wildcardread function
+#
+#     :return: a list of varnames
+#     """
+#     pattern = r'\w+\s*--\s*\w+'
+#     match = re.findall(pattern, inputstring)
+#     if not match:
+#         newlist = [s.strip() for s in inputstring.split(';')]
+#     else:
+#         for index, item in enumerate(match):
+#             inputstring = inputstring.replace(item, '__' + str(index) + '__')
+#         aloneitem = inputstring.split(';')
+#         for index, item in enumerate(match):
+#             newlist = [item if s.strip() == '__' + str(index) + '__' else s.strip() for s in aloneitem]
+#
+#     # noinspection PyUnboundLocalVariable
+#     return newlist
 
 def str2list(inputstring: str) -> Union[List[str], List[Union[str, Any]]]:
     temp_list = [s.strip() for s in inputstring.split(';')] #切分成list （默认只会存在header和main_part）
